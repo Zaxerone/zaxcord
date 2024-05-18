71a1f1c6dce9a1227ac8baf9894943edd6ddd1ce
Zaxerone <silverxenoverse2@gmail.com>
2024-05-19 01:19:07 +0200

    add dotenv

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const { ZxClient, GatewayIntentBits } = require("../index.js");
 
 const client = new ZxClient({
   intents: [GatewayIntentBits.Guilds],
-  token: "Your Token Here",
+  token: process.env.TOKEN,
   commandsPath: "./test/my-commands",
   eventsPath: "./test/my-events",
   createHandlerFolders: true,
